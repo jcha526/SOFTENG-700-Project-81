7e90cb68afe02f1c5a6ad9dfac6d6ba94597ac81
itsyefan <ethanyipnz@gmail.com>
2023-06-09 15:18:53 +1200

    feat: create login page with card

---
+++ frontend/screens/pages/LoginPage.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Text, View } from 'react-native';
+import LoginCard from '../components/LoginCard';
+
+export default function LoginPage() {
+  return (
+    <View>
+      <View className="bg-sky-600 h-2/5 flex-auto items-center justify-center">
+        <Text className="text-7xl text-white font-bold">Trackr</Text>
+      </View>
+      <View className="pt-14 flex-auto items-center justify-center">
+        <LoginCard/>
+      </View>
+    </View>
+  )
+}
