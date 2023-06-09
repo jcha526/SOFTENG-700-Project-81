3097c1f3fdf6f9072f4edc889aaa56f999c773d3
itsyefan <ethanyipnz@gmail.com>
2023-06-09 14:58:17 +1200

    feat: create card component

--- frontend/App.js
+++ frontend/App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { Text, View } from 'react-native';
+import LoginCard from './screens/components/LoginCard';
 
 export default function App() {
   return (
     <View className="flex-1 items-center justify-center ">
-      <Text className="text-amber-600">Open up App.js to start working on your asdf!</Text>
+      <Text className="text-amber-600">Open up App.js to start working on your af!</Text>
       <StatusBar style="auto" />
+      <LoginCard/>
     </View>
   );
 }
