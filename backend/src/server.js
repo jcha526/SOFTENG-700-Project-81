35420c82d22ad8ba115b491c854f75d9d996875f
itsyefan <ethanyipnz@gmail.com>
2023-07-06 19:23:21 +1200

    feat: init socket

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,9 @@
-import mongoose from 'mongoose';
-import * as dotenv from 'dotenv';
-import express from 'express';
-import routes from './routes/index.js'
-import cors from 'cors';
+import mongoose from "mongoose";
+import * as dotenv from "dotenv";
+import express from "express";
+import routes from "./routes/index.js";
+import cors from "cors";
+import dgram from "node:dgram";
 
 const app = express();
 app.use(cors());
@@ -10,14 +11,28 @@ app.use(express.json());
 const port = process.env.PORT || 3000;
 dotenv.config();
 
-app.use('/', routes);
+app.use("/", routes);
+
+const server = dgram.createSocket("udp4");
+
+server.on("listening", () => {
+  const address = server.address();
+  console.log(`server listening ${address.address}:${address.port}`);
+});
+
+server.on("message", (msg, rinfo) => {
+  console.log("Recieved UDP message");
+});
+
+server.bind(4000, process.env.IPv4_ADDRESS);
 
 app.listen(port, () => {
-    mongoose.connect(process.env.MONGO_URI)
-    .then(() => {console.log(`Listening on port ${port}`);})
+  mongoose
+    .connect(process.env.MONGO_URI)
+    .then(() => {
+      console.log(`Listening on port ${port}`);
+    })
     .catch((error) => {
-        console.log(error);
+      console.log(error);
     });
-})
-
-
+});
